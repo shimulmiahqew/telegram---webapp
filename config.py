b30ca62e328b30efad16f43e7726b729879646ea
md shimul <bitcoinhash42@gmail.com>
2025-03-05 07:48:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("7732391797:AAE2588vDmx0umob1fObZaKtYDA6H_UIwbg")
-WEBHOOK = os.getenv("Money-Airdrop.onrender.com")
-ADMIN_ID = os.getenv("7070160570")
+BOT_TOKEN = os.getenv(7732391797:AAE2588vDmx0umob1fObZaKtYDA6H_UIwbg)
+WEBHOOK = os.getenv(Money-Airdrop.onrender.com)
+ADMIN_ID = os.getenv(7070160570)

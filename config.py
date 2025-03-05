26ef9b0b709f09a357ed561eceafad48b00083ad
md shimul <bitcoinhash42@gmail.com>
2025-03-05 07:17:37 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv(7732391797:AAE2588vDmx0umob1fObZaKtYDA6H_UIwbg)
+WEBHOOK = os.getenv(Money 🤑 Airdrop.onrender.com)
+ADMIN_ID = os.getenv(7732391797)

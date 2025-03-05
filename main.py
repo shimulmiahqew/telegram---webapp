51680a5b8433dc872d9cad90d907f5e8cd52edb4
md shimul <bitcoinhash42@gmail.com>
2025-03-05 06:52:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Retrieve the token from environment variable
-BOT_TOKEN = os.getenv('BOT_TOKEN = "7732391797:AAE2588vDmx0umob1fObZaKtYDA6H_UIwbg")
+BOT_TOKEN = os.getenv("7732391797:AAE2588vDmx0umob1fObZaKtYDA6H_UIwbg")
 
                      
                      
